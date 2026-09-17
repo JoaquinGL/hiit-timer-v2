0a88704e1e3aea1dc5f858ea7f3d721c57aad212
Joaquin <joaquing@hanzo.es>
2026-09-17 10:04:50 +0200

    Publish privacy and support pages

--- src/components/c-DottedRule/c-DottedRule.svelte
+++
@@ -1,9 +0,0 @@
-<hr class="c-DottedRule" />
-
-<style lang="scss">
-  .c-DottedRule {
-    border: none;
-    border-top: 1px dotted var(--ink);
-    margin: 2rem 0;
-  }
-</style>
--- src/lib/stores/appStore.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-
-export type AppScreen = 'config' | 'workout' | 'finished';
-
-export const appStore = writable<AppScreen>('config');
